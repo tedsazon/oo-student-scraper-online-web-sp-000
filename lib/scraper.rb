1e3512db70b4a13ab164d8298530b46cf834aaa2
Ted Sazon <tedsazon@gmail.com>
2019-03-26 01:57:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     student = {}
     profile_page = Nokogiri::HTML(open(profile_url))
-    links = profile_page.css(".social-icon-container").children.css()
+    links = profile_page.css(".social-icon-container").children.css("a").map 
   end
 
 end
